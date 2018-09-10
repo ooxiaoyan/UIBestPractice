9883b04f6511265ad443cb65ba80f2dd484988d6
Qian Wang <810978527@qq.com>
2018-09-10 17:55:40 +0800

    Modify constants type from private to public

--- app/src/main/java/com/example/uibestpractice/Msg.java
+++ app/src/main/java/com/example/uibestpractice/Msg.java
@@ -5,8 +5,8 @@ public class Msg {
     private String content;
     private int type;
 
-    private static final int TYPE_RECEIVED = 0;
-    private static final int TYPE_SENT = 1;
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
 
     public Msg(String content, int type) {
         this.content = content;
