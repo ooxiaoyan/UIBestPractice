afe377b21aec6ca35a309e9ac8edcab6b620fa86
Qian Wang <810978527@qq.com>
2018-09-10 18:15:23 +0800

    Initialize some data for RecyclerView and add event response to the send button

--- app/src/main/java/com/example/uibestpractice/MainActivity.java
+++ app/src/main/java/com/example/uibestpractice/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.uibestpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button send;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter msgAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initMsgs();
+        inputText = findViewById(R.id.input_text);
+        send = findViewById(R.id.send);
+        msgRecyclerView = findViewById(R.id.msg_recycler_view);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        msgAdapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(msgAdapter);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if (!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    msgAdapter.notifyItemInserted(msgList.size() - 1);//Refresh display in RecyclerView when have new message
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);//Locate RecyclerView to the last line
+                    inputText.setText("");//Empty the contents of the input
+                }
+            }
+        });
+    }
+
+    private void initMsgs() {
+        Msg msg1 = new Msg("Hello guy.", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("Hello, who is that?", Msg.TYPE_SENT);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("This is Amy. Nice talking to you", Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
     }
 }
